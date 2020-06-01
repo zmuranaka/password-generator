1704f03045201d4cdbd5d18ec193ce654a10f8e2
zmuranaka <zmuranaka@gmail.com>
2020-06-01 11:07:50 -0600

    Rewrote JavaScript using jQuery

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 ## What is this Project?
 
-This is a simple web application that generates random passwords. The user can choose to generate passwords 1 to 64 characters long and include a mix of lowercase and uppercase letters, numbers, and symbols. No symbols that require alt codes are used. The site is published on my GitHub Pages at https://zmuranaka.github.io/password-generator/.
+This is a simple web application that utilizes jQuery to generate random passwords. The user can choose to generate passwords 1 to 64 characters long and include a mix of lowercase and uppercase letters, numbers, and symbols. No symbols that require alt codes are used. The site is published on my GitHub Pages at https://zmuranaka.github.io/password-generator/.
 
 ## Directories:
 
 images  
 &nbsp;&nbsp;&nbsp;&nbsp;This folder contains the social media logos.  
 scripts  
-&nbsp;&nbsp;&nbsp;&nbsp;This folder contains the JavaScript that generates the passwords.  
+&nbsp;&nbsp;&nbsp;&nbsp;This folder contains the JavaScript (using jQuery) that generates the passwords.  
 styles  
 &nbsp;&nbsp;&nbsp;&nbsp;This folder contains the stylesheet for the website.
 
@@ -20,7 +20,9 @@ Websites that helped me create the project:
 http://www.asciitable.com/  
 &nbsp;&nbsp;&nbsp;&nbsp;This website was a helpful reference when checking what ASCII codes corresponded to which characters.  
 https://jshint.com/  
-&nbsp;&nbsp;&nbsp;&nbsp;JSHint was a helpful tool when checking that my JavaScript syntax was correct.  
+&nbsp;&nbsp;&nbsp;&nbsp;JSHint was a helpful tool when checking that my JavaScript syntax was correct. 
+https://api.jquery.com/  
+&nbsp;&nbsp;&nbsp;&nbsp;The jQuery API was a helpful tool for checking my jQuery functions and general syntax.  
 https://www.w3schools.com/  
 &nbsp;&nbsp;&nbsp;&nbsp;W3Schools was very helpful when I was checking that my syntax was correct and as a general reference.
 
--- scripts/generator.js
+++ scripts/generator.js
@@ -1,58 +1,60 @@
-"use strict";
-
 /*
 File: generator.js
 Zachary Muranaka
-Generates a random password based on the user's input
+Generates a random password based on the user's input using jQuery
 */
 
-document.getElementById("submitBtn").addEventListener("click", generatePassword);
-
-// Returns a random member of the argument array as a char
-function randomChar(array) { return String.fromCharCode(array[Math.floor(Math.random() * array.length)]); }
-
-// Returns the password length if it is within the acceptable range or 0 if it is not
-function validPasswordLength()
+$(function()
 {
-    return (document.getElementById("numberOfChars").value > 0 && document.getElementById("numberOfChars").value < 65) ?
-    document.getElementById("numberOfChars").value : 0;
-}
+    "use strict";
 
-// Generates a password and displays the results
-function generatePassword()
-{
-    // ASCII codes for lowercase letters
-    var charRange =
-    [
-        97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109,
-        110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122
-    ];
-
-    document.getElementById("password").textContent = ""; // Reset the textContent
-
-    if(document.getElementById("nums").checked)
+    // Generates a password and displays the results
+    $('#submitBtn').click(
+    function()
     {
-        // Push the ASCII codes for numbers onto charRange
-        for(let i = 48; i <= 57; i++) charRange.push(i);
-    }
-    if(document.getElementById("mixCase").checked)
-    {
-        // Push the ASCII codes for uppercase letters onto charRange
-        for(let i = 65; i <= 90; i++) charRange.push(i);
-    }
-    if(document.getElementById("symbols").checked)
-    {
-        // Push the ASCII codes for symbols onto charRange
-        for(let i = 33; i <= 47; i++) charRange.push(i);
-        for(let i = 58; i <= 64; i++) charRange.push(i);
-        for(let i = 91; i <= 96; i++) charRange.push(i);
-        for(let i = 123; i <= 126; i++) charRange.push(i);
-    }
+        // ASCII codes for lowercase letters
+        const charRange =
+        [
+            97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109,
+            110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122
+        ];
 
-    // Generate and display the random password
-    for(let i = 0; i < validPasswordLength(); i++)
-        document.getElementById("password").textContent += randomChar(charRange);
-    
-    if(document.getElementById("password").textContent === "")
-        document.getElementById("password").textContent = "Passwords must be between 1 and 64 characters";
-}
+        $('#password').text('');
+
+        if($('#nums').is(':checked'))
+        {
+            // Push the ASCII codes for numbers onto charRange
+            for(let i = 48; i <= 57; i++) charRange.push(i);
+        }
+        if($('#mixCase').is(':checked'))
+        {
+            // Push the ASCII codes for uppercase letters onto charRange
+            for(let i = 65; i <= 90; i++) charRange.push(i);
+        }
+        if($('#symbols').is(':checked'))
+        {
+            // Push the ASCII codes for symbols onto charRange
+            for(let i = 33; i <= 47; i++) charRange.push(i);
+            for(let i = 58; i <= 64; i++) charRange.push(i);
+            for(let i = 91; i <= 96; i++) charRange.push(i);
+            for(let i = 123; i <= 126; i++) charRange.push(i);
+        }
+
+        // Generate and display the random password
+        for(let i = 0; i < validPasswordLength(); i++)
+            $('#password').append(randomChar(charRange));
+        
+        if($('#password').text() === '')
+            $('#password').text("Passwords must be between 1 and 64 characters");
+    });
+
+    // Returns a random member of the argument array as a char
+    function randomChar(array) { return String.fromCharCode(array[Math.floor(Math.random() * array.length)]); }
+
+    // Returns the password length if it is within the acceptable range or 0 if it is not
+    function validPasswordLength()
+    {
+        return ($('#numberOfChars').val() > 0 && $('#numberOfChars').val() < 65) ?
+        $('#numberOfChars').val() : 0;
+    }
+});
